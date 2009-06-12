d19039647051943916e8f4d12868f61fa24804c3
Cedric Hourcade <ced@wal.fr>
2009-06-12 12:55:38 +0200

    Moved to github

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,23 @@
-default_run_options[:pty] = true
+set :application, "wefrag"
 
-set :runner,   :cedric
-set :user,     'cedric'
-set :use_sudo, false
+set :scm, :git
+set :repository, "git@github.com:hc/wefrag.git"
+
+set :branch, "stable"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
 
 role :app, "wefrag.com"
 role :web, "wefrag.com"
 role :db,  "wefrag.com", :primary => true
+set :deploy_to, "/home/cedric/#{application}/production"
 
-set :application, "wefrag"
+default_run_options[:pty] = true
 
+set :runner,   :cedric
+set :user,     "cedric"
+set :use_sudo, false
 
 task :after_symlink do
   run "ln -nfs #{shared_path}/medias #{current_path}/public/medias"
@@ -17,10 +25,19 @@ task :after_symlink do
   run "ln -nfs #{shared_path}/tmp/attachment_fu #{current_path}/tmp/attachment_fu"
 end
 
-set :stages, %w(production)
-require 'capistrano/ext/multistage'
+task :start, :roles => [:web] do
+  sudo "/etc/init.d/thin start", :as => :root
+end
 
-deploy.task :after_update_code, :roles => [:web] do
+task :stop, :roles => [:web] do
+  sudo "/etc/init.d/thin stop", :as => :root
+end
+
+task :restart, :roles => [:web] do
+  sudo "/etc/init.d/thin restart", :as => :root
+end
+
+task :after_update_code, :roles => [:web] do
   desc "Link in the production database.yml"
   [ 'database', 'config' ].each do |file|
     run "ln -nfs #{shared_path}/config/#{file}.yml #{release_path}/config/#{file}.yml"
