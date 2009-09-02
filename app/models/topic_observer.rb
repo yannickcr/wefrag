85976593608fb3df578bbe663593cff67e0de4cc
Cedric Hourcade <ced@wal.fr>
2009-09-02 12:24:44 +0200

    last post at

--- app/models/topic_observer.rb
+++ app/models/topic_observer.rb
@@ -1,4 +1,8 @@
 class TopicObserver < ActiveRecord::Observer
+  def after_create(topic)
+    topic.update_attribute(:last_post_at, topic.created_at)
+  end
+
   def after_update(topic)
     Forum.find(topic.forum_id_was).touch if topic.forum_id_changed?
   rescue ActiveRecord::RecordNotFound
