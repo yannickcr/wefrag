4f3455840d8089bc109f47122a29d49a64fe734c
Cedric Hourcade <ced@wal.fr>
2009-09-02 10:57:34 +0200

    fix nofrag redirection

--- app/controllers/nofrag_controller.rb
+++ app/controllers/nofrag_controller.rb
@@ -1,5 +1,5 @@
 class NofragController < ApplicationController
-  def news
+  def show
     item = ImportedNofragItem.posted.find_by_remote_id(params[:id]) or raise ActiveRecord::RecordNotFound
     redirect_to forum_topic_url(item.topic.forum, item.topic)
   end
