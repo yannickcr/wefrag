ceea91dd6dc5096bd2d12710af5858cbede5445b
Cedric Hourcade <ced@wal.fr>
2009-09-25 11:49:41 +0200

    search controller changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,10 @@
  class SearchController < ApplicationController
-  before_filter :sanitize_params
-
   def new
-    @string = ''
+    @string = ""
   end
 
   def create
-    @string = URI.escape(params[:s])
+    @string = URI.escape("#{params[:s]}")
 
     unless @string.empty?
       redirect_to search_url(@string)
@@ -16,38 +14,24 @@
   end
 
   def show
-    @string = URI.unescape(params[:s])
+    @string = URI.unescape("#{params[:s]}")
+
     forums    = logged_in? ? current_user.readable_forums : Group.anonymous.readable_forums
     forum_ids = forums ? forums.collect { |f| f.id } : []
 
-    unless !defined?(Ultrasphinx) or forum_ids.empty? or @string.empty? or params[:s].size < 3
-      Ultrasphinx::Search.excerpting_options[:before_match] = '[b]'
-      Ultrasphinx::Search.excerpting_options[:after_match]  = '[/b]'
-      Ultrasphinx::Search.excerpting_options[:limit]        = 512
-      Ultrasphinx::Search.excerpting_options[:around]       = 3
-      Ultrasphinx::Search.excerpting_options[:content_methods] = ['body']
-
+    unless !defined?(Ultrasphinx) or forum_ids.empty? or @string.empty? or @string.size < 3
       @search = Ultrasphinx::Search.new(
-        :query     => '@@relaxed ' + @string,
-        :page      => params[:page],
-        :sort_mode => 'descending',
-        :sort_by   => 'created_at',
+        :query     => "@@relaxed #{@string}",
+        :page      => "#{params[:page]}".to_i,
         :filters   => { 'forum_id' => forum_ids }
       )
 
       @search.excerpt
-      render_404 if params[:page] > [1, @search.page_count].max
+      render_404 if @search.current_page > [1, @search.page_count].max
     else
       @search = nil
     end
   rescue Ultrasphinx::DaemonError
     @search = nil
-  end
-
-  private
-
-  def sanitize_params
-    self.params[:s]    = params[:s].to_s
-    self.params[:page] = params[:page].to_s.to_i
   end
 end
--- config/initializers/ultrasphinx.rb
+++ config/initializers/ultrasphinx.rb
@@ -1,3 +1,28 @@
 if defined?(Ultrasphinx)
   Ultrasphinx::Search.client_options[:finder_methods].unshift(:find_with_includes)
+
+  Ultrasphinx::Search.query_defaults = HashWithIndifferentAccess.new({
+    :query       => nil,
+    :page        => 1,
+    :per_page    => 25,
+    :sort_by     => 'created_at',
+    :sort_mode   => 'descending',
+    :indexes     => [
+      Ultrasphinx::MAIN_INDEX,
+      (Ultrasphinx::DELTA_INDEX if Ultrasphinx.delta_index_present?)
+    ].compact,
+    :weights     => {},
+    :class_names => [],
+    :filters     => {},
+    :facets      => []
+  })
+
+  Ultrasphinx::Search.excerpting_options = HashWithIndifferentAccess.new({
+    :before_match    => '[b]',
+    :after_match     => '[/b]',
+    :chunk_separator => '...',
+    :limit           => 512,
+    :around          => 3,
+    :content_methods => ['body']
+  })
 end
